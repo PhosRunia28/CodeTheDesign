5e80d245d9ef7948d0930f70da2504206f52ff27
PhosRunia28 <tediphos28@gmail.com>
2024-06-01 21:53:48 +0700

    refactor: Suxz (Image Performance, import aliases, cn util, fix prettier)

--- Suxz/src/components/Btn.tsx
+++ Suxz/src/components/Btn.tsx
@@ -1,36 +1,27 @@
-type BtnProps = {
-  outline?: boolean;
-  muted?: boolean;
-  className?: string;
-  children: React.ReactNode;
-  to: string;
-};
+import { cn } from '@utils/util'
 
-export default function Btn({
-  outline,
-  muted,
-  className,
-  children,
-  to,
-}: BtnProps) {
-  return (
-    <a
-      href={to}
-      className={
-        `inline-block rounded-lg border px-6 py-2 transition  
-      ${
-        outline
-          ? "text-primary hover:border-primary border-white hover:bg-white"
-          : "bg-primary border-primary hover:text-primary text-white hover:bg-white"
-      } ${
-        muted
-          ? "hover:text-primary border-white bg-gray-100 text-gray-500 hover:bg-gray-100"
-          : ""
-      } ` + className
-      }
-      //   "bg-primary border-primary hover:text-primary inline-block rounded-lg border px-6 py-2 text-white transition hover:bg-white"
-    >
-      {children}
-    </a>
-  );
+type BtnProps = {
+	outline?: boolean
+	muted?: boolean
+	className?: string
+	children: React.ReactNode
+	to: string
+}
+
+export default function Btn({ outline, muted, className, children, to }: BtnProps) {
+	return (
+		<a
+			href={to}
+			className={cn(
+				`inline-block rounded-lg border border-primary bg-primary px-6 py-2 text-white transition hover:bg-white hover:text-primary`,
+				{
+					'border-white bg-gray-100 text-gray-500 hover:bg-gray-100 hover:text-primary': muted,
+					'border-white bg-white text-primary hover:border-primary hover:bg-white': outline
+				},
+				className
+			)}
+		>
+			{children}
+		</a>
+	)
 }
--- Suxz/tailwind.config.mjs
+++ Suxz/tailwind.config.mjs
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {
-      colors: {
-        primary: "#8C82FC",
-        secondary: "#F8F8FF",
-      },
-      fontFamily: {
-        manrope: ["Manrope", "sans-serif"],
-      },
-    },
-  },
-  plugins: [],
-};
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				primary: '#8C82FC',
+				secondary: '#F8F8FF'
+			},
+			fontFamily: {
+				manrope: ['Manrope', 'sans-serif']
+			}
+		}
+	},
+	plugins: []
+}
